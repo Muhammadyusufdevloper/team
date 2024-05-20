e96621b406930a9415ed9a6a949a92223bfc45cd
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-05-20 12:42:58 +0500

    muhammadyusf

---
+++ src/pages/muhammadyusuf/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Muhammadyusuf.scss"
+
+const Muhammadyusuf = () => {
+  return (
+    <div className='wrapper'>
+        <h1>Muhammadyusuf</h1>
+    </div>
+  )
+}
+
+export default Muhammadyusuf
